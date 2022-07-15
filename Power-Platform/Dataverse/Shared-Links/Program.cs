d267205eec607331e05e7003a0a3740ab376b379
Peter Hecke <pehecke@microsoft.com>
2022-07-14 18:18:40 -0700

    Initial code

---
+++ Power-Platform/Dataverse/Shared-Links/Program.cs
@@ -0,0 +1,260 @@
+﻿#define PREVIEW // SharedLink preview release. Limited set of tables supported.
+
+using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.Xrm.Sdk;
+
+namespace PowerPlatform.Dataverse.CodeSamples
+{
+    /// <summary>
+    /// Demonstrates table row shared link operations.
+    /// </summary>
+    /// <remarks>Set the appropriate Url and Username values for your test
+    /// environment in the appsettings.json file before running this program.</remarks>
+    /// <see cref="https://docs.microsoft.com/power-apps/developer/data-platform/xrm-tooling/use-connection-strings-xrm-tooling-connect#connection-string-parameters"/>
+    /// <permission cref="https://github.com/microsoft/PowerApps-Samples/blob/master/LICENSE"
+    /// <author>Peter Hecke</author>
+    class Program
+    {
+        /// <summary>
+        /// Contains the application's configuration settings. 
+        /// </summary>
+        IConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Storage for table row references created by this program for later use.
+        /// </summary>
+        private static Dictionary<string,EntityReference> entityStore = new();
+
+        /// <summary>
+        /// Constructor. Loads the application configuration settings from a JSON file.
+        /// </summary>
+        Program()
+        {
+            // Get the path to the appsettings file. If the environment variable is set,
+            // use that file path. Otherwise, use the runtime folder's settings file.
+            string? path = Environment.GetEnvironmentVariable("DATAVERSE_APPSETTINGS");
+            if (path == null) path = "appsettings.json";
+
+            // Load the app's configuration settings from the JSON file.
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(path, optional: false, reloadOnChange: true)
+                .Build();
+        }
+
+        static void Main(string[] args)
+        {
+            Program app = new();
+
+            // Create a Dataverse service client using the default connection string.
+            ServiceClient client =
+                new(app.Configuration.GetConnectionString("default"));
+
+            app.Setup(client); // Create initial required resources.
+
+            // Obtain information about the table row created in Setuo().
+            entityStore.TryGetValue("Fourth Coffee", out EntityReference? account);
+
+            if( account != null )
+            {
+                // Enable link sharing on the table row, then create a shared link
+                // with read-write access to the row.
+                app.Enable_SharedLink(client, account);
+                Console.WriteLine(
+                    "Shared links enabled for {0} '{1}'", account.LogicalName, account.Name);
+
+                Guid linkId = app.Create_SharedLink(client, account, 
+                    AccessRights.ReadAccess | AccessRights.WriteAccess);
+                Console.WriteLine(
+                    "Shared link created for {0} '{1}'", account.LogicalName, account.Name);
+
+                //Retrieve all shared links for the table row.
+                EntityCollection rowSharedLinks = app.Retrieve_SharedLinks(client, account);
+                Console.WriteLine(
+                    "Shared links for {0} '{1}'", account.LogicalName, account.Name);
+                for(int i=0; i<rowSharedLinks.TotalRecordCount; i++)
+                {
+                    Console.WriteLine("\tShared link [ID:{0}]", rowSharedLinks.Entities[i].Id);
+                }
+
+                // Revoke write access to the table row.
+                app.Revoke_SharedLink(client, account, AccessRights.WriteAccess);
+                Console.WriteLine("Write access revoked for shared link [ID:{0}].", linkId);
+            }
+            else
+            {
+                Console.WriteLine("Account was not found..exiting.");
+            }
+
+            // Pause program execution before resource cleanup.
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+
+            app.Cleanup(client); // Delete any created resources.
+            client.Dispose();
+        }
+
+        /// <summary>
+        /// Creates any initial table rows or other resources required by the program.
+        /// </summary>
+        /// <param name="client">The service client.</param>
+        /// <remarks>The entityStore is a global dictionary of table row references.</remarks>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.powerplatform.dataverse.client.serviceclient.create"/>
+        public void Setup(ServiceClient client)
+        {
+            // A new account row.
+            Guid id = client.Create(
+                new Entity("Fourth Coffee") 
+                    { LogicalName = "account" }
+            );
+            entityStore.Add("Fourth Coffee", new EntityReference("account", id));
+        }
+
+        /// <summary>
+        /// Enables user access to a table row using a shared link.
+        /// </summary>
+        /// <param name="client">A ServiceClient instance.</param>
+        /// <param name="entityId">The target table row.</param>
+        /// <remarks>The entityStore is a global dictionary of table row references.</remarks>
+        /// <see cref="https://docs.microsoft.com/power-apps/developer/data-platform/reference/entities/sharedlinksetting"/>
+        public void Enable_SharedLink(ServiceClient client, EntityReference entRef)
+        {
+        #if PREVIEW
+            if(entRef.LogicalName != "account"  && entRef.LogicalName != "contact" &&
+               entRef.LogicalName != "case" && entRef.LogicalName != "opportunity")
+            {
+                throw new Exception(
+                    string.Format("Table '{0}' does not currently support shared links.",
+                    entRef.LogicalName));
+            };
+        #endif
+
+            Entity row = new("sharedlinksetting");
+            row.Attributes = new AttributeCollection()
+            {
+                {"extensionofrecordid", entRef },
+                {"isenabledforsharedlinkcreation", true }
+            };
+
+            Guid settingId = client.Create(row);
+            entityStore.Add("shareLink", new EntityReference("sharedlinksetting", settingId));
+        }
+
+        /// <summary>
+        /// Creates a shared link for a table row.
+        /// </summary>
+        /// <param name="client">A ServiceClient instance.</param>
+        /// <param name="entRef">The target table row.</param>
+        /// <param name="rights">Access rights for the table row.</param>
+        /// <returns>Unique identifier of the shred link.</returns>
+        /// <remarks>The entityStore is a global dictionary of table row references.</remarks>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.crm.sdk.messages.generatesharedlinkrequest"/>
+        public Guid Create_SharedLink(ServiceClient client, EntityReference entRef,
+            AccessRights rights)
+        {
+            GenerateSharedLinkRequest request = new()
+            {
+                Target = entRef,
+                SharedRights = rights   
+            };
+
+            GenerateSharedLinkResponse response = 
+                (GenerateSharedLinkResponse)client.Execute(request);
+
+            EntityReference linkRef = new("shareLink", (Guid)response.Results["shareLink"]);
+            entityStore.Add("shareLink", linkRef);
+
+            return linkRef.Id;
+        }
+
+        /// <summary>
+        /// Retrieve all shared links on the table row.
+        /// </summary>
+        /// <param name="client">A ServiceClient instance.</param>
+        /// <param name="entRef">The target table row.</param>
+        /// <returns>A collection of shared links for the table row.</returns>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.crm.sdk.messages.retrievesharedlinksrequest"/>
+        public EntityCollection Retrieve_SharedLinks(ServiceClient client, 
+            EntityReference entRef)
+        {
+            RetrieveSharedLinksRequest request = new() { Target = entRef };
+
+            RetrieveSharedLinksResponse response =
+                (RetrieveSharedLinksResponse)client.Execute(request);
+
+            return response.SharedLinks;
+        }
+
+        /// <summary>
+        /// Adds the calling user to the shared link access team of the table row.
+        /// </summary>
+        /// <param name="client">A ServiceClient instance.</param>
+        /// <param name="entRef">The target table row.</param>
+        /// <param name="linkId">The shared link ID.</param>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.crm.sdk.messages.grantaccessusingsharedlinkrequest"/>
+        public void GrantAccessUsing_SharedLink(ServiceClient client, 
+            EntityReference entRef, Guid linkId)
+        {
+            Uri uri = new(String.Format(
+                "https://{0}/main.aspx?pagetype=entityrecord&etn={1}&id={2}&shareLink={3}",
+                client.ConnectedOrgUriActual.Host,
+                entRef.LogicalName, entRef.Id, linkId ));
+
+            GrantAccessUsingSharedLinkRequest request = new()
+            { RecordUrlWithSharedLink = uri.ToString() };
+
+            GrantAccessUsingSharedLinkResponse response =
+                (GrantAccessUsingSharedLinkResponse)client.Execute(request);
+        }
+
+        /// <summary>
+        /// Revokes the specified user access rights from the shared link. 
+        /// </summary>
+        /// <param name="client">A ServiceClient instance.</param>
+        /// <param name="entRef">The target table row.</param>
+        /// <param name="rights">Access rights to revoke.</param>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.crm.sdk.messages.revokesharedlinkrequest"/>
+        public void Revoke_SharedLink(ServiceClient client, EntityReference entRef,
+            AccessRights rights)
+        {
+            RevokeSharedLinkRequest request = new()
+            {
+                Target = entRef,
+                SharedRights = rights
+            };
+            
+            RevokeSharedLinkResponse response = 
+                (RevokeSharedLinkResponse)client.Execute(request);
+        }
+
+        /// <summary>
+        /// Delete all table rows created by this program in reverse order of creation.
+        /// </summary>
+        /// <param name="client">The service client.</param>
+        /// <remarks>The entityStore is a global dictionary of table row references.</remarks>
+        /// <see cref="https://docs.microsoft.com/dotnet/api/microsoft.powerplatform.dataverse.client.serviceclient.delete"/>
+        public void Cleanup(ServiceClient client)
+        {
+            while(entityStore.Count> 0)
+            {
+                KeyValuePair<string,EntityReference> kv = entityStore.Last();
+                var key = kv.Key;  var entRef = kv.Value;
+
+                try
+                {
+                    entityStore.Remove(key);
+                    client.Delete(entRef.LogicalName, entRef.Id);
+                }
+                catch( Exception ex)
+                {
+                    Console.WriteLine(
+                        "Table '{0}' row '{1}' could not be deleted..skipping.",
+                        entRef.LogicalName, entRef.Name);
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+        }
+    }
+}
---
+++ Power-Platform/Dataverse/Shared-Links/README.md
@@ -0,0 +1,36 @@
+﻿# Getting started with shared links to table rows
+
+This code sample demonstrates using the Microsoft Dataverse shared links messages. The shared links feature allows the caller to share a link to a table row with another user. 
+
+More information: [Sharing and assigning](https://docs.microsoft.com/power-apps/developer/data-platform/security-sharing-assigning)
+
+The provided code samples are listed below.
+
+|Sample folder|Description|Build target|
+|---|---|---|
+|shared-links|Demonstrates enabling, creating, retrieving, and revoking a shared link.|.NET 6|
+
+## Instructions
+
+1. Download the solution folder from GitHub.
+
+1. Open the *shared-links.sln* solution file in Visual Studio 2022.
+
+1. Edit the *appsettings.json* file in the **Solution Items** folder of Solution Explorer. Set the connection string `Url` and `Username` parameters as appropriate for your test environment.
+
+	The environment Url can be found in the Power Platform admin center. It has the form https://\<environment-name>.crm.dynamics.com.
+
+1. Build the solution, and then run the project.
+
+When the code sample runs, you will be prompted in the default browser to select an environment user account and enter a password. To avoid having to do this every time you run a sample, insert a password parameter into the connection string in the appsettings.json file. For example:
+
+```json
+{
+"ConnectionStrings": {
+    "default": "AuthType=OAuth;Url=https://myorg.crm.dynamics.com;Username=someone@myorg.onmicrosoft.com;Password=mypassword;RedirectUri=http://localhost;AppId=51f81489-12ee-4a9e-aaae-a2591f45987d;LoginPrompt=Auto"
+  }
+}
+```
+
+**Tip**: You can set a user environment variable named DATAVERSE_APPSETTINGS to the file path of the appsettings.json file stored anywhere on your computer. The samples will use that appsettings file if the environment variable exists and is not null. Be sure to log out and back in again after you define the variable for it to take affect. To set an environment variable, go to **Settings > System > About**, select **Advanced system settings**, and then choose **Environment variables**. 
+
