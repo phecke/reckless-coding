425130ad7ea5b8526750d3b87945200bbdcd1b3d
Peter Hecke <pehecke@microsoft.com>
2024-03-05 18:37:30 -0800

    New coding projects

---
+++ Sample code vNext/Sample Class Library/Interfaces/ISample.cs
@@ -0,0 +1,27 @@
+﻿namespace GettingStarted
+{
+    /// <summary>
+    /// Defines an interface for a code sample.
+    /// </summary>
+    public interface ISample
+    {
+        /// <summary>
+        /// Performs any setup or resource allocation required by but not performed in the Run() method.
+        /// </summary>
+        /// <returns>Created data or other allocated resource.</returns>
+        public virtual object? Setup() { return null; }
+
+        /// <summary>
+        /// Performs the main function of the code sample.
+        /// </summary>
+        /// <param name="resource">Data or other objects required but not created in this method.</param>
+        /// <returns>True if successful; otherwise false.</returns>
+        public virtual bool Run(object? resource) { return false; }
+
+        /// <summary>
+        /// Frees up any resources allocated by the Setup() method.
+        /// </summary>
+        /// <param name="resource">Allocated resources.</param>
+        public virtual void Cleanup(object? resource) { }
+    }
+}
