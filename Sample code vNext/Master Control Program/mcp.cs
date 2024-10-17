53ce2b3edf34e9119ea99dbadd608dece150d9ed
Peter Hecke <pehecke@microsoft.com>
2024-10-17 10:59:17 -0700

    Code enhancements

--- Sample code vNext/Master Control Program/mcp.cs
+++ Sample code vNext/Master Control Program/mcp.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.Xrm.Sdk;
 using GettingStarted;
+using Microsoft.Extensions.Configuration; 
 
 namespace Master_Control_Program
 {
@@ -9,52 +10,62 @@ namespace Master_Control_Program
     /// </summary>
     internal class mcp
     {
-        #region Cloud service connection
-        // TODO Place this info in a settings.json file.
-        static string url = "https://myorg.crm.dynamics.com";
-        static string userName = "someone@myorg.onmicrosoft.com";
-        static string password = "mypassword";
+        /// <summary>
+        /// Contains the application's configuration settings. 
+        /// </summary>
+        IConfiguration Configuration { get; }
 
-        // This service connection string uses the info provided above.
-        // The AppId and RedirectUri used here are provided for sample code testing.
-        static string connectionString = $@"
-            AuthType = OAuth;
-            Url = {url};
-            UserName = {userName};
-            Password = {password};
-            AppId = 51f81489-12ee-4a9e-aaae-a2591f45987d;
-            RedirectUri = app://58145B91-0C36-4500-8554-080854F2AC97;
-            LoginPrompt=Auto;
-            RequireNewInstance = True";
-        #endregion Cloud service connection
+        /// <summary>
+        /// Constructor. Loads the application configuration settings from a JSON file.
+        /// </summary>
+        mcp()
+        {
+            // Get the path to the appsettings file. If the environment variable is set,
+            // use that file path. Otherwise, use the runtime folder's settings file.
+            string? path = Environment.GetEnvironmentVariable("DATAVERSE_APPSETTINGS");
+            if (path == null) path = "appsettings.json";
+
+            // Load the app's configuration settings from the JSON file.
+            Configuration = new ConfigurationBuilder()
+                .AddJsonFile(path, optional: false, reloadOnChange: true)
+                .Build();
+        }
 
         static void Main(string[] args)
         {
+            EntityCollection entityStore;
+            IPowerSample app;
+
+            mcp mcpApp = new();
+
             try
             {
-                // Connect to the Dataverse web service.
-                ServiceClient client = new ServiceClient(connectionString);   
+                // Create a Dataverse service client using the default connection string.
+                ServiceClient serviceClient =
+                   new(mcpApp.Configuration.GetConnectionString("default"));
+
+                app = new CreateUpdateDelete();
+                entityStore = app.Setup(serviceClient);
 
                 try
                 {
-                    IPowerSample app = new CreateUpdateDelete();
-                    EntityCollection entityStore = app.Setup(client);
-
-                    if (app.Run(client, entityStore) == false)
-                        Console.WriteLine(typeof(CreateUpdateDelete) + 
-                            ".Run() method did not complete successfully.");
-
-                    app.Cleanup(client, entityStore);
+                    if (app.Run(serviceClient, entityStore) == false)
+                        Console.WriteLine(typeof(CreateUpdateDelete) +
+                                ".Run() method did not complete successfully.");
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine(typeof(CreateUpdateDelete) + " terminated with an exception.");
                     Console.WriteLine(ex.Message);
                 }
+                finally
+                {
+                    app.Cleanup(serviceClient, entityStore);
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("MCP was unable to establish a connection with the web service");
+                Console.WriteLine("The program unable to establish a connection with the Dataverse web service.");
                 Console.WriteLine(ex.Message);
             }
 
